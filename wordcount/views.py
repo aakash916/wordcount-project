60d973d8720da9998f8b215616810241653aff57
Aakash Gupta <aakash916@gmail.com>
2021-01-03 17:16:10 +0530

    This is first commit

---
+++ wordcount/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
+import operator
+
+
+def home(request):
+	return render(request, 'home.html') 
+
+def eggs(request):
+	return HttpResponse('<h2>EggS!!!</h2>') 
+
+def count(request):
+	fulltext= request.GET['fulltext']
+	wordlist = fulltext.split()
+	wordDict = {}
+
+	for word in wordlist:
+		if word not in wordDict.keys():
+			wordDict[word] = 1
+		else:
+			wordDict[word] += 1
+
+	sortedwords = sorted(wordDict.items(), key=operator.itemgetter(1), reverse=True)
+	return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist), 'sortedwords': sortedwords}) 
+
+
+def about(request):
+	return HttpResponse('Welcome to my site!')
